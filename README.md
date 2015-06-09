c4702fde116539378c6fe4ffd3ab1fb877eaf238
przepiora <e.przepiorka@gmail.com>
2015-06-10 00:08:34 +0200

    zaimportowalam prawidlowo tabele, zrobilam plota, ktory wyglada jak plot

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # zuza_model_regresji
+
+Log:
+
+9.06.2015
+
+* tabele zaimportowalam przez read.csv2
+* zmienna "average_BPM" figuruje jako numeric
+* plot wyglada jak rasowy plot, ale zapomnialam, jakie wartosci mialam tam zmienic
+* nadal nie wiem, co oznaczaja wyniki wyplute prze 13 i 15 linie kodu (predict)
+
+Do zrobienia:
+
+9.06.2015
+
+* napisac cos takiego, co pozwoli wyliczyc temperature [nietoperza] na podstawie podanej wartosci "average_BPM"
+* zrozumiec dokladnie, co wnosza poszczegolne linie kodu w skrypcie
--- regression_model_zuza.R
+++ regression_model_zuza.R
@@ -1,25 +1,22 @@
 ## Posilkowalam sie ksiazka "Introduction to R" Szymona Drobniaka
 # robilam wg wzoru z rozdzialu "Simple linear models"
 
-bipniecia <- read.csv("reg_model_Zuza.csv", sep = ";", header = T)
+## Zmienna "average_BPM" figuruje jako numeric
+
+bipniecia <- read.csv2("reg_model_Zuza.csv", sep = ";", header = T)
 
 bipniecia.lm <- with(bipniecia, lm(temperature~average_BPM))
 
 summary(bipniecia.lm)
 
-predict(bipniecia.lm)
-
-class(bipniecia.lm)
-
-attributes(bipniecia.lm)
-
 predict(bipniecia.lm) [2:4]
 
 bipniecia$temperature [2:4] - predict(bipniecia.lm) [2:4]
 
+# Teraz plot wyglada ladnie
 with(bipniecia, plot(temperature~average_BPM, type="l", las = 1))
 
+## Chcialabym wiedziec, po co to jest? Ale przekopiowalam to z przykladu z ksiazki 
+# z mysla, ze zobacze, co mi to wyrzuci. Cos wyrzucilo, ale nie wiem, co.
 x2 <- seq(0, 50, 2)
 y2 <- predict(bipniecia.lm, list(temperature=x2), int="c")
-
-## I na tym etapie sie wysypalam, bo nie wiem co dalej robic :)
